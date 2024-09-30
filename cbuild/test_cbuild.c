a6cd44579981691778cb44c306242692bc7fa3ce
Mohamed A. Elmeligy <mohamed.a.elmeligy@protonmail.com>
2024-09-30 10:23:47 +0300

    cbuild: use utest for testing

--- cbuild/test_cbuild.c
+++ cbuild/test_cbuild.c
@@ -1,46 +1,54 @@
 #include <cbuild.h>
 #include <cbuild_private.h>
 
-#include <assert.h>
+#include <utest.h>
+
 #include <stdio.h>
 #include <stdlib.h>
 
 #define STR(s) s, (sizeof (s) - 1)
 
-int
-main (void)
+UTEST_F_SETUP (CBuild)
 {
-  CBuild cbuild;
-  CError err = cbuild_create (CBUILD_TYPE_debug, ".", 1, &cbuild);
-  assert (err.code == 0);
+  CError err = cbuild_create (CBUILD_TYPE_debug, ".", 1, utest_fixture);
+  ASSERT_EQ_MSG (err.code, 0, err.msg);
+}
 
+UTEST_F_TEARDOWN (CBuild)
+{
+  ASSERT_TRUE (utest_fixture);
+  cbuild_destroy (utest_fixture);
+}
+
+UTEST_F (CBuild, general)
+{
   CTarget target;
-  err = cbuild_exe_create (&cbuild, STR ("t"), &target);
-  assert (err.code == 0);
+  CError err = cbuild_exe_create (utest_fixture, STR ("t"), &target);
+  ASSERT_EQ_MSG (err.code, 0, err.msg);
 
-  err =
-      cbuild_target_add_source (&cbuild, &target, STR ("build/cbuild/main.c"));
-  assert (err.code == 0);
+  err = cbuild_target_add_source (
+      utest_fixture, &target, STR ("build/cbuild/main.c")
+  );
+  ASSERT_EQ_MSG (err.code, 0, err.msg);
 
   CTarget target2;
-  err = cbuild_shared_lib_create (&cbuild, STR ("t2"), &target2);
-  assert (err.code == 0);
+  err = cbuild_shared_lib_create (utest_fixture, STR ("t2"), &target2);
+  ASSERT_EQ_MSG (err.code, 0, err.msg);
 
-  err =
-      cbuild_target_add_source (&cbuild, &target2, STR ("build/cbuild/main.c"));
-  assert (err.code == 0);
+  err = cbuild_target_add_source (
+      utest_fixture, &target2, STR ("build/cbuild/main.c")
+  );
+  ASSERT_EQ_MSG (err.code, 0, err.msg);
 
   CTarget target3;
-  err = cbuild_static_lib_create (&cbuild, STR ("t3"), &target3);
-  assert (err.code == 0);
+  err = cbuild_static_lib_create (utest_fixture, STR ("t3"), &target3);
+  ASSERT_EQ_MSG (err.code, 0, err.msg);
 
-  err =
-      cbuild_target_add_source (&cbuild, &target3, STR ("build/cbuild/main.c"));
-  assert (err.code == 0);
+  err = cbuild_target_add_source (
+      utest_fixture, &target3, STR ("build/cbuild/main.c")
+  );
+  ASSERT_EQ_MSG (err.code, 0, err.msg);
 
-  err = cbuild_build (&cbuild);
-  assert (err.code == 0);
-
-  cbuild_destroy (&cbuild);
-  return 0;
+  err = cbuild_build (utest_fixture);
+  ASSERT_EQ_MSG (err.code, 0, err.msg);
 }
